1ee26bac1c4e9adcb4d6e409dcd70487880b8714
Yu-Hao Chang <9203700@gmail.com>
2018-09-29 10:39:44 +0800

    change architecture

--- Project_1/hw1_0416005.cpp
+++ Project_1/hw1_0416005.cpp
@@ -31,10 +31,8 @@ int global_timer;
 int guarantee_number_counter;
 // Store "YES" and "NO" strings
 vector<string> yes_no_string_list;
-// Store each player next arrive time, index is player id
-vector<unsigned int> arrive_time;
 // Indicate if the claw machine is playing or not
-bool is_playing;
+bool is_claw_machine_playing;
 // Indicate is the current player is done or not
 bool current_player_done;
 // Record the if of the player who is currently playing
@@ -44,15 +42,17 @@ bool can_detach;
 
 // Shared with all player threads
 queue<unsigned int> waiting_queue;
-// Semaphore used by each player thread and master thread
-// as a lock for shared resource waiting_queue
-sem_t* waiting_queue_write_mutex;
 
 // Data that transfer to master thread
 typedef struct master_thread_data {
   USER_INFO* user_info_array;
 } MASTER_THREAD_DATA;
 
+// Semaphore used by each player thread and master thread
+// as a lock for shared resource waiting_queue
+sem_t* waiting_queue_write_mutex;
+// Semaphore used by each player thread as a lock for shared resouce player_id
+sem_t* player_id_mutex;
 // Semaphore used by each player thread as a lock for shared IO resource
 sem_t* io_mutex;
 // Semaphores used by master thread for waking up each player thread
@@ -98,6 +98,7 @@ USER_INFO* file_input(const char* filename) {
 
 void init_semaphore() {
   waiting_queue_write_mutex = new sem_t;
+  player_id_mutex = new sem_t;
   io_mutex = new sem_t;
   player_start_sem_array = new sem_t[number_of_players];
   player_done_sem_array = new sem_t[number_of_players];
@@ -105,6 +106,7 @@ void init_semaphore() {
   all_start_sem = new sem_t;
   all_complete_sem = new sem_t;
   sem_init(waiting_queue_write_mutex, 0, 1);
+  sem_init(player_id_mutex, 0, 1);
   sem_init(io_mutex, 0, 1);
   for(unsigned int i = 0; i < number_of_players; ++i) {
     sem_init(player_start_sem_array + i, 0, 0);
@@ -117,6 +119,7 @@ void init_semaphore() {
 
 void free_semaphore() {
   if (waiting_queue_write_mutex != NULL) delete waiting_queue_write_mutex;
+  if (player_id_mutex != NULL) delete player_id_mutex;
   if (io_mutex != NULL) delete io_mutex;
   if (player_start_sem_array != NULL) delete[] player_start_sem_array;
   if (player_done_sem_array != NULL) delete[] player_done_sem_array;
@@ -127,13 +130,15 @@ void free_semaphore() {
 
 void* master_thread_routine(void* thread_data) {
   MASTER_THREAD_DATA* _thread_data = (MASTER_THREAD_DATA*) thread_data;
-  bool is_previous_timeslot_occupied = false;
+
   global_timer = _thread_data->user_info_array[0].arrive_time;
   number_of_finished_players = 0;
-  is_playing = false;
+  player_id = 0;
+  is_claw_machine_playing = false;
   current_player_done = false;
   can_detach = false;
   guarantee_number_counter = guarantee_number;
+
   sem_wait(all_start_sem);
   sem_post(msg_order_sem);
   while(number_of_finished_players < number_of_players) {
@@ -144,17 +149,26 @@ void* master_thread_routine(void* thread_data) {
       sem_wait(player_done_sem_array + i);
     }
     sem_wait(msg_order_sem);
-    if (is_previous_timeslot_occupied && waiting_queue.empty()) {
+
+    if (!is_claw_machine_playing) {
       guarantee_number_counter = guarantee_number;
-      is_previous_timeslot_occupied = false;
     }
+
+    // Check if there's any finished player in this timeslot
     if (current_player_done) {
-      // Remove the the player that just finished his round
-      sem_wait(waiting_queue_write_mutex);
+      // If there is any player on the waiting queue,
+      // assign him to the claw machine that is finished by
+      // another player in this timeslot, so the waiting player
+      // will know that he can play by checking
+      // the variable player_id
+      if (!waiting_queue.empty()) {
+        player_id = waiting_queue.front();
         waiting_queue.pop();
-      sem_post(waiting_queue_write_mutex);
-      is_playing = false;
-      is_previous_timeslot_occupied = true;
+      }
+      else {
+        player_id = 0;
+      }
+      is_claw_machine_playing = false;
     }
     else {
       sem_post(msg_order_sem);
@@ -176,20 +190,45 @@ void* master_thread_routine(void* thread_data) {
 
 void* player_thread_routine(void* thread_data) {
   USER_INFO* user_info = (USER_INFO*)thread_data;
-  int rest_rounds_once = user_info->number_of_times_once;
-  int rest_rounds = user_info->total_play_rounds;
+  int rest_continuous_rounds = user_info->number_of_times_once;
+  int rest_total_rounds = user_info->total_play_rounds;
+  // True means the player is either playing or waiting
+  // False, otherwise
+  bool already_line_up = false;
+  /****************************************
+   * True means the player has got prize  *
+   *    or finished his continuous rounds *
+   * False, otherwise                     *
+   ****************************************/
   bool leave_or_rest = false;
   bool get_prize = false;
+  /****************************************
+   * 0-based, indicate which claw machine *
+   *    is playing by the player.         *
+   * if its value is -1, means the player *
+   *    is not playing on a claw machine  *
+   ****************************************/
+  int claw_machine_id = -1;
+  unsigned int arrive_time = user_info->arrive_time;
+
   while(true) {
     sem_wait(player_start_sem_array + (user_info->id - 1));
+    // Check if the player has got a prize
+    // or finished his continuous rounds at previous timeslot
+    // so he can print the finish messages at first
     if(leave_or_rest) {
       sem_wait(io_mutex);
         cout << global_timer << " " << user_info->id << " ";
         cout << FINISH_MESSAGE << yes_no_string_list[get_prize] << endl;
       sem_post(io_mutex);
       leave_or_rest = false;
+      claw_machine_id = -1;
+      already_line_up = false;
       sem_post(msg_order_sem);
     }
+    // This routine will halt until the finish messages is printed,
+    // this make the finish message is printed before any start 
+    // and wait messages
     else {
       sem_wait(msg_order_sem);
       sem_post(msg_order_sem);
@@ -197,59 +236,65 @@ void* player_thread_routine(void* thread_data) {
     if(can_detach) break;
 
     if(!get_prize) {
-      if( (int)arrive_time[user_info->id - 1] == global_timer ) {
-        // Player should start playing or start waiting line
-        sem_wait(waiting_queue_write_mutex);
-          waiting_queue.push(user_info->id);
-        sem_post(waiting_queue_write_mutex);
-        if (waiting_queue.front() == user_info->id) {
-          // The player can start playing directly since he's the first who occupy the claw machine
-          is_playing = true;
+      if( (int)arrive_time == global_timer ) {
+        // Check if the claw machine is not occupied
+        // by checking the player_id is zero or not
+        sem_wait(player_id_mutex);
+        if (player_id == 0) {
+          claw_machine_id = 0;
           player_id = user_info->id;
+          is_claw_machine_playing = true;
+          already_line_up = true;
           sem_wait(io_mutex);
             cout << global_timer << " " << user_info->id << " ";
             cout << START_MESSAGE << endl;
           sem_post(io_mutex);
         }
-        else {
-          /* *****************************************
-           * The arrived player can't start playing  *
-           * since there's someone ahead of hime in  *
-           * the line.                               *
-           * This player can only push his ID to the *
-           * waiting queue.                          *
-           * *****************************************/
+        sem_post(player_id_mutex);
+
+        // If the claw machine is occupied, join himself to waiting queue
+        if (!already_line_up) {
+          sem_wait(waiting_queue_write_mutex);
+            waiting_queue.push(user_info->id);
+          sem_post(waiting_queue_write_mutex);
+          already_line_up = true;
           sem_wait(io_mutex);
             cout << global_timer << " " << user_info->id << " ";
             cout << WAIT_MESSAGE << endl;
           sem_post(io_mutex);
         }
       }
-      else if (!is_playing 
-        && waiting_queue.front() == user_info->id 
-        && (int)arrive_time[user_info->id - 1] <= global_timer) {
-        // The player that is start playing after line up
-        is_playing = true;
-        player_id = user_info->id;
-        sem_wait(io_mutex);
-          cout << global_timer << " " << user_info->id << " ";
-          cout << START_MESSAGE << endl;
-        sem_post(io_mutex);
+      // The player who is waiting will enter this section
+      else if (global_timer > (int)arrive_time && claw_machine_id < 0) {
+        // Exhaustive check if the claw machine is not occupied
+        sem_wait(player_id_mutex);
+        if (player_id == user_info->id) {
+          claw_machine_id = 0;
+          is_claw_machine_playing = true;
+          sem_wait(io_mutex);
+            cout << global_timer << " " << user_info->id << " ";
+            cout << START_MESSAGE << endl;
+          sem_post(io_mutex);
+        }
+        sem_post(player_id_mutex);
       }
       else {}
-      // Player now is playing or resting or waiting
-      if (is_playing && player_id == user_info->id) {
-        --rest_rounds_once;
-        --rest_rounds;
+      // Player has to do something after one play with the claw machine
+      if (claw_machine_id >= 0) {
+        --rest_continuous_rounds;
+        --rest_total_rounds;
         --guarantee_number_counter;
-        get_prize = guarantee_number_counter == 0 || (rest_rounds == 0);
-        current_player_done = get_prize || rest_rounds_once == 0;
-        leave_or_rest = current_player_done;
+
+        get_prize = guarantee_number_counter == 0 || (rest_total_rounds == 0);
         guarantee_number_counter = (get_prize) ? guarantee_number : guarantee_number_counter;
-        if (current_player_done) {
+        current_player_done = get_prize || rest_continuous_rounds == 0;
+        leave_or_rest = current_player_done;
+
+        // Player has finished his coutinuous round or game
+        if (leave_or_rest) {
           number_of_finished_players = (get_prize) ? number_of_finished_players + 1 : number_of_finished_players;
-          arrive_time[user_info->id - 1] = (get_prize) ? global_timer : global_timer + 1 + user_info->rest_time ;
-          rest_rounds_once = user_info->number_of_times_once;
+          arrive_time = (get_prize) ? global_timer : global_timer + 1 + user_info->rest_time ;
+          rest_continuous_rounds = user_info->number_of_times_once;
         }
       }
     }
@@ -264,17 +309,14 @@ int main(int argc, char* argv[]) {
   USER_INFO* user_info_array;
   user_info_array = file_input(argv[1]);
 
+  if (!user_info_array) return 0;
+
   MASTER_THREAD_DATA* _thread_data = new MASTER_THREAD_DATA;
   thread_array.resize(number_of_players + 1);
   _thread_data->user_info_array = user_info_array;
-  arrive_time.resize(number_of_players);
   sort(user_info_array, user_info_array + number_of_players, compare_function_with_arrive_time);
   yes_no_string_list.push_back(" NO");
   yes_no_string_list.push_back(" YES");
-
-  for(unsigned int i = 0; i < number_of_players; ++i) {
-    arrive_time[ user_info_array[i].id - 1 ] = user_info_array[i].arrive_time;
-  }
 
   init_semaphore();
 
