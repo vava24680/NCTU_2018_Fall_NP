6c6cb57b18af440c934034aa5dfd032e0805a6a9
Yu-Hao <9203700@gmail.com>
2020-02-23 11:04:14 +0800

    Project 5

--- Project_5/server/tar.sh
+++ Project_5/server/tar.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-tar -cvf  application_server.tar --transform 's,^,application_server/,' amqp_client_thread/amqp_client_thread.py amqp_client_thread/__init__.py application_server.py base_server/base_server.py base_server/__init__.py application_server_requirement.txt config/server_config.yml
+mkdir application_server
+tar -cvf application_server/application_server.tar --transform 's,^,application_server/,' amqp_client_thread/amqp_client_thread.py amqp_client_thread/__init__.py application_server.py base_server/base_server.py base_server/__init__.py application_server_requirement.txt config/server_config.yml
